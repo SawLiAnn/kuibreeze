0a970aea05c759a3b4de138602ea548c6d4a4ec0
liannsaw <liannsaw16@gmail.com>
2022-12-23 00:12:36 +0800

    change dashboard logo url to home

--- resources/views/components/sidebar/header.blade.php
+++ resources/views/components/sidebar/header.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center justify-between flex-shrink-0 px-3">
     <!-- Logo -->
-    <a href="{{ route('projects.index') }}" class="inline-flex items-center gap-2">
+    <a href="/" class="inline-flex items-center gap-2">
         <x-application-logo aria-hidden="true" class="w-10 h-auto" />
     </a>
 
